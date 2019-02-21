472e6c8e5a7cc7ddd9aaa339121e3941d3202566
João Thallis <joaothallis.developer@gmail.com>
2019-02-20 23:45:38 -0300

    Add dotfiles

---
+++ .zshrc
@@ -0,0 +1,80 @@
+alias cat='pygmentize -g'
+alias p="postman"
+alias tm="tmux new -s"
+alias ra="ranger"
+alias cop="xclip -sel clip <"
+alias e="&&"
+alias cu="curl"
+alias sud="sudo"
+alias t="trans en:pt -b"
+alias pt="trans pt:en -b"
+alias i="feh"
+alias fe="firefox"
+alias z="source ~/.zshrc"
+alias co="mix coveralls.html && firefox cover/excoveralls.html"
+alias de="date"
+alias c="cal -y"
+# Network manager
+alias n="nmtui"
+alias dot='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias m="mix"
+
+alias kc='docker kill $(docker ps -q)'
+alias ds='docker ps'
+
+alias gl=gloga
+alias gp="git pull"
+alias gpf="git push -f"
+alias gpfn="git push -f --no-verify"
+alias gbd="git branch -D"
+alias gck="git checkout"
+alias gckc="git checkout -b"
+alias gpr="git pull --rebase"
+alias grl="git rebase -"
+alias gcm="git checkout master && git pull --rebase"
+alias gr="git reset HEAD~1"
+alias gc="git checkout"
+alias gre="git rebase -"
+alias gs="git status"
+
+alias ku='kubectx "$(kubectx | fzy)"'
+alias k='kubens "$(kubens | fzy)"'
+alias ge="kubectl get endpoints"
+
+# Youtube CLI
+alias my="mpsyt"
+
+export KUBECONFIG=$HOME/k8s/kub-config-inside-hml
+export ZSH="/home/joao/.oh-my-zsh"
+export EDITOR=code
+export VISUAL=code
+export TERM="xterm-256color"
+
+. $HOME/.asdf/asdf.sh
+. $HOME/.asdf/completions/asdf.bash
+
+ZSH_THEME=crunch
+plugins=(git kubectl web-search)
+source $ZSH/oh-my-zsh.sh
+unsetopt correct_all
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/joao/Downloads/google-cloud-sdk/path.zsh.inc' ]; then . '/home/joao/Downloads/google-cloud-sdk/path.zsh.inc'; fi
+# The next line enables shell command completion for gcloud.
+if [ -f '/home/joao/Downloads/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/joao/Downloads/google-cloud-sdk/completion.zsh.inc'; fi
+
+if [ $TILIX_ID ] || [ $VTE_VERSION ]; then
+        source /etc/profile.d/vte.sh
+fi
+
+alias kubeip=$'kubectl describe service | grep Endpoint | grep -v none | awk \'{ print $2 }\''
+alias kubepod=$'kubectl get pod --no-headers | awk \'NR==1{print $1}\''
+kubenv() { kubectl exec -it $(kubepod) -c $1 printenv }
+kubexec() { kubectl exec -it $(kubepod) -c $1 /bin/bash }
+kubeforward() { kubectl port-forward deploy/$1 5432:5432 }
+
+
+# kubectl config
+export KUBECONFIG=/home/joao/.kube/stone
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
---
+++ setup.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && \
+    sudo dpkg -i erlang-solutions_1.0_all.deb
+
+sudo add-apt-repository ppa:kelleyk/emacs
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install git tmux tilix xclip feh pgcli mycli curl ranger zsh tmux \
+     translate-shell gnustep-gui-runtime mps-youtube rofi i3 i3lock-fancy \
+     nodejs apt-transport-https ca-certificates gnupg-agent containerd.io \
+     software-properties-common fzy inotify-tools jq automake emacs26 autoconf \
+     libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev \
+     libffi-dev libtool unixodbc-dev esl-erlang elixir docker-ce docker-ce-cli \
+     python-pygments tree
+
+git clone --bare https://github.com/joao/dotfiles.git $HOME/.dotfiles
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
+
+curl -l \
+     https://gist.githubusercontent.com/lucasdavila/3875946/raw/\
+     1c100cae16a06bef154af0f290d665405b554b3b/install_source_code_pro.sh \
+    | sudo sh
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+mix do escript.install hex ex_doc, local.hex
+mix archive.install hex phx_new 1.4.1
+
+git clone https://github.com/jeremyjh/dialyxir.git
+cd dialyxir
+mix do deps.get, archive.build, archive.install
+cd -
+
+git clone https://github.com/rrrene/bunt.git
+cd bunt
+mix do archive.build, archive.install
+cd -
+git clone https://github.com/rrrene/credo.git
+cd credo
+mix do deps.get, archive.build, archive.install
+cd -
+
+sudo usermod -aG docker $USER
+sudo curl -L \
+     "https://github.com/docker/compose/releases/download/1.23.2/\
+docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+wget \
+    https://apt.iteas.at/iteas/pool/main/o/openfortigui/\
+openfortigui_0.7.2-3_bionic_amd64.deb
+sudo dpkg -i openfortigui_0.7.2-3_bionic_amd64.deb
+sudo apt-get install -f
+
+curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+rm -rf erlang-solutions_1.0_all.deb openfortigui_0.7.2-3_bionic_amd64.deb \
+   dialyxir bunt credo SourceCodePro_FontsOnly-1.010.zip \
+   SourceCodePro_FontsOnly-1.010
+
+snap install postman
+sudo snap install kubectl --classic
+sudo snap install slack --classic
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+
+sh -c \
+   "$(wget https://raw.githubusercontent.com/robbyrussell/\
+oh-my-zsh/master/tools/install.sh -O -)"
+
+git config --global user.name "João Thallis"
+git config --global user.email joaothallis.developer@gmail.com
+
+ssh-keygen
+xclip -sel clip < ~/.ssh/id_rsa.pub
+firefox https://github.com/settings/ssh/new
